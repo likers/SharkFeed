a970e330649edd83825d2fbb70c58782a8cbcf34
Jinhuan MacbookPro <likers.li33@gmail.com>
2016-08-08 21:21:18 -0500

    init commit

---
+++ SharkFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharkFeed
+//
+//  Created by Jinhuan Li on 8/8/16.
+//  Copyright © 2016 likers33. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SharkFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SharkFeed
+//
+//  Created by Jinhuan Li on 8/8/16.
+//  Copyright © 2016 likers33. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
