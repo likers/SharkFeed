36c9a389daee3236cf424b411f7f11fd1f5d2af4
Jinhuan MacbookPro <likers.li33@gmail.com>
2016-08-08 22:44:30 -0500

    add splash screen

--- SharkFeed/Controllers/HomeCollectionViewController.h
+++ SharkFeed/Controllers/HomeCollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeCollectionViewController.h
 //  SharkFeed
 //
 //  Created by Jinhuan Li on 8/8/16.
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
 
+@interface HomeCollectionViewController : UIViewController
 
 @end
-
---
+++ SharkFeed/Controllers/HomeCollectionViewController.m
@@ -0,0 +1,22 @@
+//
+//  HomeCollectionViewController.m
+//  SharkFeed
+//
+//  Created by Jinhuan Li on 8/8/16.
+//  Copyright © 2016 likers33. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HomeCollectionViewController.h"
+
+
+@implementation HomeCollectionViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+@end
